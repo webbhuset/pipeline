0005ff829622846f46659be3912dbfcaf660f256
Albert Dahlin <info@albertdahlin.com>
2017-01-20 15:38:10 +0100

    Updated state executor test

--- tests/core/UnitTest/Utils/Processor/StatementExecuterTest.php
+++ tests/core/UnitTest/Utils/Processor/StatementExecuterTest.php
@@ -8,38 +8,19 @@ class StatementExecuterTest
 
     public static function __constructTest($test)
     {
+        $logger    = new NullLogger;
+        $mockProcessor = [new Collector];
+        $test
+            ->testThatArgs($logger, $mockProcessor, [])->throws('Webbhuset\\Bifrost\\Core\\BifrostException')
+            ->testThatArgs($logger, $mockProcessor, [
+                'connection' => 'apa',
+                'statement' => 'fisk',
+            ])
+            ->throws('Webbhuset\\Bifrost\\Core\\BifrostException');
     }
 
     public static function processNextTest($test)
     {
-        require_once '/var/www/magento/magento1931/magento/app/Mage.php';
-        \Mage::app();
-        $nullLogger    = new NullLogger;
-        $collector     = new Collector;
-        $mockProcessor = [$collector];
-        $write         = \Mage::getSingleton('core/resource')->getConnection('core_write');
-        $connection    = $write->getConnection();
-        $statement     = $connection->prepare("SELECT * FROM catalog_product_entity where SKU like :sku");
-        $params        = [
-            'statement'  => $statement,
-            'connection' => $connection,
-        ];
-        $indata = [
-            'bind_values' =>[':sku' => '123123'],
-        ];
-        $test->newInstance($nullLogger, $mockProcessor, $params)
-            ->testThatArgs($indata)->returnsNull();
 
-        $result = $collector->getData();
-    }
-
-    public static function finalizeTest()
-    {
-    }
-    public static function initTest()
-    {
-    }
-    public static function countTest()
-    {
     }
 }
