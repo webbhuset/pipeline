9669f3140999ba859a1e2a5c54c67d00406f5336
Andreas Lundkvist <github@galdiuz.com>
2017-02-28 16:46:46 +0100

    Moved impl to separate project (WHIP)

--- impl/mageone/app/code/community/Webbhuset/Bifrost/sql/whbifrost_setup/install-1.0.0.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include "includes/1.0.0-log-table.php";
-include "includes/1.0.0-log-message-table.php";
-include "includes/1.0.0-eav-attribute-option-value-index.php";
-include "includes/1.0.0-eav-attribute-label-index.php";
