8b8f561e68e0653b756657cf8c715fe9afab41a2
Albert Dahlin <info@albertdahlin.com>
2017-04-02 21:31:02 +0200

    Added support for progress callback to app

--- src/App.php
+++ src/App.php
@@ -7,6 +7,7 @@ use Webbhuset\Bifrost\Core\App\JobManagerInterface;
 class App
 {
     protected $jobManager;
+    protected $progressCallbacks = [];
 
     public function __construct(JobManagerInterface $jobManager)
     {
@@ -17,8 +18,15 @@ class App
     {
         $job = $this->jobManager->getJob($job, $options);
 
+        $lastProgress = time();
+
         while (!$job->isDone()) {
             $job->processNext();
+
+            if ((time() - $lastProgress) > 2) {
+                $this->updateProgress($job);
+                $lastProgress = time();
+            }
         }
     }
 
@@ -30,5 +38,21 @@ class App
     public function getJobInfo($job)
     {
         return $this->jobManager->getJobInfo($job);
+    }
+
+    public function registerProgressCallback($callback)
+    {
+        if (!is_callable($callback)) {
+            throw new BifrostException("Callback is not callable");
+        }
+
+        $this->progressCallbacks[] = $callback;
+    }
+
+    protected function updateProgress($job)
+    {
+        foreach ($this->progressCallbacks as $callback) {
+            call_user_func($callback, $job);
+        }
     }
 }
