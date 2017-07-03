84e4862ac9777e670ae7746e3360ef2337a1104d
Albert Dahlin <info@albertdahlin.com>
2017-07-03 17:59:51 +0200

    Added args validator class

---
+++ src/Args.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Webbhuset\Whaskell;
+
+use ReflectionMethod;
+use ReflectionFunction;
+use Exception;
+
+class Args
+{
+    public static function canBeUsedWithArgCount($callable, $count, $generator = null)
+    {
+        if (!is_callable($callable)) {
+            return 'Callback is not callable';
+        }
+
+        $ref = self::getReflectionFromCallback($callable);
+
+        $requiredCount = $ref->getNumberOfRequiredParameters();
+        $optionalCount = $ref->getNumberOfParameters();
+
+        $isGenerator = $ref->isGenerator();
+
+        if ($isGenerator && $generator === false) {
+            return "You can not use a generator here.";
+        }
+
+        if (!$isGenerator && $generator === true) {
+            return "Function must be a generator. Use yield instead of return.";
+        }
+
+        if ($requiredCount > $count) {
+            return "To many args. Callback Requires {$requiredCount} arg(s), only {$count} will be passed.";
+        }
+
+        if ($optionalCount < $count) {
+            return "To few args in callback. {$count} args will be passed but callback only takes {$optionalCount} args.";
+        }
+
+        return true;
+    }
+
+    protected static function getReflectionFromCallback($callback)
+    {
+        $reflection = null;
+        if (is_array($callback)) {
+            list ($class, $method) = $callback;
+            $reflection = new ReflectionMethod($class, $method);
+        }
+
+        if (!$reflection) {
+            try {
+                $reflection = new ReflectionFunction($callback);
+            } catch (Exception $e) {
+            }
+        }
+
+        if (!$reflection) {
+            try {
+                $reflection = new ReflectionMethod($callback);
+            } catch (Exception $e) {
+            }
+        }
+
+        return $reflection;
+    }
+}
