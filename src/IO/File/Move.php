944990770de2a2ee68d00330f7c76ef4aeee3b9f
Andreas Lundkvist <github@galdiuz.com>
2017-10-16 13:31:36 +0200

    MoveFile can now use a callback to get the file to move

--- src/IO/File/Move.php
+++ src/IO/File/Move.php
@@ -8,20 +8,34 @@ use Webbhuset\Whaskell\WhaskellException;
 
 class Move extends AbstractFunction
 {
-    protected $callback;
+    protected $oldPathCallback;
+    protected $newPathCallback;
     protected $copy = false;
 
-    public function __construct($callback, $config = [])
+    public function __construct($oldPathCallback, $newPathCallback = null, $config = [])
     {
-        $canBeUsed = FunctionSignature::canBeUsedWithArgCount($callback, 1);
-
-        if ($canBeUsed !== true) {
-            throw new WhaskellException($canBeUsed . ' Eg. function($item)');
+        if (!is_callable($newPathCallback)) {
+            $oldPathCallback    = function($item) {
+                return $item;
+            };
+            $newPathCallback    = $oldPathCallback;
+            $config             = $newPathCallback ?: [];
         }
 
-        $this->callback = $callback;
+        $canBeUsed = FunctionSignature::canBeUsedWithArgCount($oldPathCallback, 1);
+        if ($canBeUsed !== true) {
+            throw new WhaskellException('$oldPathCallback: ' . $canBeUsed . ' Eg. function($item)');
+        }
 
-        if (isset($config['copy'])) {
+        $canBeUsed = FunctionSignature::canBeUsedWithArgCount($newPathCallback, 1);
+        if ($canBeUsed !== true) {
+            throw new WhaskellException('$newPathCallback: ' . $canBeUsed . ' Eg. function($item)');
+        }
+
+        $this->oldPathCallback = $oldPathCallback;
+        $this->newPathCallback = $newPathCallback;
+
+        if (array_key_exists('copy', $config)) {
             $this->copy = $config['copy'];
         }
     }
@@ -29,25 +43,23 @@ class Move extends AbstractFunction
     protected function invoke($items, $finalize = true)
     {
         foreach ($items as $item) {
-            if (!is_string($item)) {
-                $msg = 'Item is not a string.';
+            $oldPath = call_user_func($this->oldPathCallback, $item);
+
+            if (!is_string($oldPath)) {
+                $msg = 'File to move is not a string.';
                 $this->error($item, $msg);
 
-                yield $item;
                 continue;
             }
-
-            $oldPath = $item;
 
             if (!file_exists($oldPath)) {
                 $msg = "File '{$oldPath}' does not exist.";
                 $this->error($item, $msg);
 
-                yield $oldPath;
                 continue;
             }
 
-            $newPath    = call_user_func($this->callback, $oldPath);
+            $newPath    = call_user_func($this->newPathCallback, $item);
             $dir        = dirname($newPath);
 
             if (!is_dir($dir)) {
@@ -55,13 +67,11 @@ class Move extends AbstractFunction
                     $msg = "Could not create directory '{$dir}', file exists with same path.";
                     $this->error($item, $msg);
 
-                    yield $oldPath;
                     continue;
                 } elseif (!mkdir($dir, 0777, true)) {
                     $msg = "Could not create directory '{$dir}'.";
                     $this->error($item, $msg);
 
-                    yield $oldPath;
                     continue;
                 }
             }
@@ -70,10 +80,8 @@ class Move extends AbstractFunction
                 $msg = "Could not copy '{$oldPath}' to '{$newPath}'.";
                 $this->error($item, $msg);
 
-                yield $oldPath;
                 continue;
             }
-
 
             if ($this->observer) {
                 $type   = $this->copy ? 'Copied' : 'Moved';
