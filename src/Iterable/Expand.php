1b9037b417090e037437761f69431e5496df7d13
Andreas Lundkvist <github@galdiuz.com>
2018-11-20 14:07:01 +0100

    dev

--- src/Iterable/Expand.php
+++ src/Iterable/Expand.php
@@ -2,9 +2,41 @@
 
 namespace Webbhuset\Whaskell\Iterable;
 
-$phpVersion = phpversion();
+use Webbhuset\Whaskell\FunctionInterface;
+use Webbhuset\Whaskell\FunctionSignature;
+use Webbhuset\Whaskell\WhaskellException;
 
-switch (1) {
-    case version_compare($phpVersion, '7.0'): return require_once 'Expand-7.0.php';
-    case version_compare($phpVersion, '5.5'): return require_once 'Expand-5.5.php';
+class Expand implements FunctionInterface
+{
+    protected $callback;
+
+    public function __construct($callback = null)
+    {
+        if ($callback !== null) {
+            $canBeUsed = FunctionSignature::canBeUsedWithArgCount($callback, 1, true);
+
+            if ($canBeUsed !== true) {
+                throw new WhaskellException($canBeUsed . ' e.g. function($item)');
+            }
+        } else {
+            $callback = function($items) {
+                foreach ($items as $item) {
+                    yield $item;
+                }
+            };
+        }
+
+        $this->callback = $callback;
+    }
+
+    public function __invoke($items, $finalize = true)
+    {
+        foreach ($items as $item) {
+            $generator = call_user_func($this->callback, $item);
+
+            foreach ($generator as $yield) {
+                yield $yield;
+            }
+        }
+    }
 }
